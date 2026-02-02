96b3cd9b665102a5d25a51883ecc8f70237134a2
mateteandrew4-gif <mateteandrew4@gmail.com>
2026-02-02 03:45:10 -0800

    Update send-receipt.js

--- api/send-receipt.js
+++ api/send-receipt.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -49,4 +49,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: 'Server error', details: error.message });
   }
-}
+};
