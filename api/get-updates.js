ee3696231e81a0cf4e12cf71327bb6d0d392d6e0
mateteandrew4-gif <mateteandrew4@gmail.com>
2026-02-02 03:05:57 -0800

    Update get-updates.js

--- api/get-updates.js
+++ api/get-updates.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -79,6 +81,6 @@ export default async function handler(req, res) {
     });
 
   } catch (error) {
-    return res.status(500).json({ error: 'Server error' });
+    return res.status(500).json({ error: 'Server error', details: error.message });
   }
 }
